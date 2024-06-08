6b40097e000e3d885828e4085814f9be7a0b0a38
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-06-07 23:52:40 -0600

    Added first recovery

--- src/recovery1.jsx
+++ src/recovery1.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text , TextInput , TouchableOpacity} from "react-native";
+import Styles from "../assets/css/styles"
+import { useNavigation } from "@react-navigation/native";
 
 const Recovery1 = () => {
+  const navigation = useNavigation();
+  const signup = () => {
+    navigation.navigate('Signup');
+  };
+  const recovery2 = () => {
+   navigation.navigate('Recovery2');
+  };
+
   return (
     <View>
-      <Text>Recovery1</Text>
-      <Button title="Recovery1" onPress={() => {}} />
+      <Text style={Styles.title}>Recuperar cuenta</Text>
+      <Text style={Styles.subtitle}>Ingrese su correo electrónico para recuperar cuenta</Text>
+      <TextInput style={Styles.input} placeholder="multimedios@gmail.com" />
+      <TouchableOpacity onPress={signup}>
+        <Text style={Styles.link}>Iniciar sesión</Text>
+      </TouchableOpacity>
+      <TouchableOpacity  onPress={recovery2} style={Styles.buttonContainer}>
+        <Text style={Styles.button}>Continuar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={signup}>
+        <Text style={Styles.linkAux}>¿No tienes una cuenta?</Text>
+        <Text style={Styles.linkAux1}>Regístrate</Text>
+      </TouchableOpacity>
     </View>
   );
 }
